37404df87cbe9e6c4c35bec340728a427bd4f837
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-13 19:31:50 +0530

    added basic navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,16 @@
 const Header = (): JSX.Element => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <nav className="flex justify-between px-4 py-2 bg-[#29274C]">
+        <div className="">
+          <h1 className="">Shoppy Globe</h1>
+        </div>
+        <div>
+          <button>Cart</button>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
