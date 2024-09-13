b6a1004e614ad6073177efbce6ef3ddce4f8195e
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-13 22:40:19 +0530

    added function to fetch data from api

---
+++ src/hooks/useFetchProducts.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  thumbnail: string;
+}
+
+const useFetchProducts = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/products");
+        const data = await response.json();
+        setProducts(data.products);
+      } catch (err:any) {
+       return err.message
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  return { products, loading };
+};
+
+export default useFetchProducts;
