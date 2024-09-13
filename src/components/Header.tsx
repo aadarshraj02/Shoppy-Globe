2473814c336af308de97523ff922a18eac6939e4
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-13 19:55:13 +0530

    added basic nav bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,18 @@
 const Header = (): JSX.Element => {
   return (
     <header>
-      <nav className="flex justify-between px-4 py-2 bg-[#1995AD] text-zinc-100">
+      <nav className="flex justify-between items-center px-4 py-2 bg-[#1995AD] text-zinc-100">
         <div className="">
-          <h1 className="">Shoppy Globe</h1>
+          <h1 className="text-2xl font-bold">
+            Shoppy <span className="text-zinc-700 font-normal">Globe </span>{" "}
+          </h1>
         </div>
-        <div>
+        <div className="flex items-center gap-6">
+          <ul className="flex gap-6">
+            <li>About</li>
+            <li>Contact Us</li>
+            <li>Login</li>
+          </ul>
           <button>Cart</button>
         </div>
       </nav>
