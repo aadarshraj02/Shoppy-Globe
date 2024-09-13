aab15471a637013be250041e2d77a7511bbb2971
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-13 10:13:50 +0530

    imported and added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Header from "./components/Header";
+
 const App = (): JSX.Element => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
