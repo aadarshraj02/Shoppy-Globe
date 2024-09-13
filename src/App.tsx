2eb70eed09bd893589fcc8f5791bb95e36ba3a7d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-13 20:55:57 +0530

    imported and added ProductList.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import ProductList from "./pages/ProductList";
 
 const App = (): JSX.Element => {
   return (
     <div>
       <Header />
+      <ProductList />
     </div>
   );
 };
