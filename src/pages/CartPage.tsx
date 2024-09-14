1d6262e976126f6cb3494ce150228252597f3f32
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-14 10:40:48 +0530

    added CartPage

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,5 +1,33 @@
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../redux/store";
+import {
+  incrementQty,
+  decrementQty,
+  removeFromCart,
+} from "../redux/slices/CartSlice";
+
 const CartPage = (): JSX.Element => {
-  return <div>CartPage</div>;
+  const cartItems = useSelector((state: RootState) => state.cart.cart);
+  const dispatch = useDispatch();
+
+  const handleIncrement = (id: number) => {
+    dispatch(incrementQty(id));
+  };
+
+  const handleDecrement = (id: number) => {
+    dispatch(decrementQty(id));
+  };
+
+  const handleRemove = (id: number) => {
+    dispatch(removeFromCart(id));
+  };
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4 text-center">Your Cart</h1>
+      {cartItems.length === 0 ? <p>Your cart is empty.</p> : <div></div>}
+    </div>
+  );
 };
 
 export default CartPage;
