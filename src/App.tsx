1042bddf99fd744807f3d7b18b80ddc53dec7525
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-14 23:52:25 +0530

    added routes for about contact and login

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import About from "./pages/About";
 import CartPage from "./pages/CartPage";
 import CheckoutPage from "./pages/Checkout";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
 import OrderSuccess from "./pages/OrderSuccess";
 import ProductDetails from "./pages/ProductDetails";
 import ProductList from "./pages/ProductList";
@@ -18,6 +21,9 @@ const App = (): JSX.Element => {
           <Route path="/checkout" element={<CheckoutPage />} />
           <Route path="/order" element={<OrderSuccess />} />
           <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
         <Footer />
       </BrowserRouter>
