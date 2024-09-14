a90f12550f0ae4f753e9f99fcf0e273917dc8b57
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-15 00:22:29 +0530

    applied react lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,43 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import About from "./pages/About";
-import CartPage from "./pages/CartPage";
-import CheckoutPage from "./pages/Checkout";
-import Contact from "./pages/Contact";
-import ErrorPage from "./pages/ErrorPage";
-import Login from "./pages/Login";
-import OrderSuccess from "./pages/OrderSuccess";
-import ProductDetails from "./pages/ProductDetails";
-import ProductList from "./pages/ProductList";
+import React, { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { InfinitySpin } from "react-loader-spinner";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const About = React.lazy(() => import("./pages/About"));
+const CartPage = React.lazy(() => import("./pages/CartPage"));
+const CheckoutPage = React.lazy(() => import("./pages/Checkout"));
+const Contact = React.lazy(() => import("./pages/Contact"));
+const ErrorPage = React.lazy(() => import("./pages/ErrorPage"));
+const Login = React.lazy(() => import("./pages/Login"));
+const OrderSuccess = React.lazy(() => import("./pages/OrderSuccess"));
+const ProductDetails = React.lazy(() => import("./pages/ProductDetails"));
+const ProductList = React.lazy(() => import("./pages/ProductList"));
 
 const App = (): JSX.Element => {
   return (
     <>
       <BrowserRouter>
         <Header />
-        <Routes>
-          <Route path="/" element={<ProductList />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/order" element={<OrderSuccess />} />
-          <Route path="/products/:id" element={<ProductDetails />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
+        <Suspense
+          fallback={
+            <div className="flex justify-center items-center h-[60vh]">
+              <InfinitySpin width="200" color="#1995AD" />
+            </div>
+          }
+        >
+          <Routes>
+            <Route path="/" element={<ProductList />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/order" element={<OrderSuccess />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </Suspense>
         <Footer />
       </BrowserRouter>
     </>
