8028d2b0ea4429daaeef710b2050aad5ce780c8a
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-14 10:57:35 +0530

    styled the empty cart

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,10 +1,12 @@
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../redux/store";
+import { FaShoppingCart } from "react-icons/fa";
 import {
   incrementQty,
   decrementQty,
   removeFromCart,
 } from "../redux/slices/CartSlice";
+import { Link } from "react-router-dom";
 
 const CartPage = (): JSX.Element => {
   const cartItems = useSelector((state: RootState) => state.cart.cart);
@@ -23,10 +25,29 @@ const CartPage = (): JSX.Element => {
   };
 
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4 text-center">Your Cart</h1>
-      {cartItems.length === 0 ? <p>Your cart is empty.</p> : <div></div>}
-    </div>
+    <>
+      <div className="p-4">
+        <h1 className="text-3xl font-bold mb-4 flex items-center justify-center gap-2">
+          Your Cart <FaShoppingCart />
+        </h1>
+        {cartItems.length === 0 ? (
+          <>
+            <p className="text-red-500 text-2xl text-center mt-10">
+              Your cart is empty.
+            </p>
+            <div className="flex items-center justify-center h-[40vh]">
+              <Link to="/">
+                <button className="bg-[#1995AD] hover:bg-[#356570] transition-all duration-300 ease-linear text-white px-4 py-2 mt-2 rounded flex items-center gap-1">
+                  Browse Products to add
+                </button>{" "}
+              </Link>
+            </div>
+          </>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </>
   );
 };
 
