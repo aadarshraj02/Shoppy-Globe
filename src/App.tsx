6fc2a2b96c08c40183ef1bcb4d0bd820106bbd47
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-14 10:46:47 +0530

    added routes for product page and cart page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import Header from "./components/Header";
+import CartPage from "./pages/CartPage";
 import ProductList from "./pages/ProductList";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = (): JSX.Element => {
   return (
-    <div>
-      <Header />
-      <ProductList />
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
