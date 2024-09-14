4a05dd4c41f17f4e8baea01b265c9539f40e4d74
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-14 19:28:08 +0530

    added link to get the product detail according to id

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -2,6 +2,7 @@ import { FaShoppingCart } from "react-icons/fa";
 import { IoIosStar } from "react-icons/io";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../redux/slices/CartSlice";
+import { Link } from "react-router-dom";
 
 interface ProductItemProps {
   id: number;
@@ -38,21 +39,23 @@ const ProductItem = ({
 
   return (
     <div className="border p-4 rounded-lg shadow hover:shadow-lg transition-all duration-200 ease-linear cursor-pointer">
-      <img
-        src={image}
-        alt={name}
-        className="h-56 w-full  bg-[#A1D6E2] mb-3 rounded-lg"
-      />
-      <h3 className="text-xl font-bold">{shortedTitle(name)}</h3>
-      <p className="text-gray-600">
-        <span className="text-black">Price: $</span>
-        {price}
-      </p>
-      <p className="text-xs text-gray-500 flex items-center gap-1">
-        {" "}
-        <span className="text-black">Rating:</span>{" "}
-        <IoIosStar className="text-yellow-500" /> {rating}
-      </p>
+      <Link to={`/products/${id}`}>
+        <img
+          src={image}
+          alt={name}
+          className="h-56 w-full  bg-[#A1D6E2] mb-3 rounded-lg"
+        />
+        <h3 className="text-xl font-bold">{shortedTitle(name)}</h3>
+        <p className="text-gray-600">
+          <span className="text-black">Price: $</span>
+          {price}
+        </p>
+        <p className="text-xs text-gray-500 flex items-center gap-1">
+          {" "}
+          <span className="text-black">Rating:</span>{" "}
+          <IoIosStar className="text-yellow-500" /> {rating}
+        </p>
+      </Link>
       <button
         onClick={() => handleAddToCart(id, name, price, image)}
         className="bg-[#1995AD] hover:bg-[#356570] transition-all duration-300 ease-linear text-white px-4 py-2 mt-2 rounded flex items-center gap-1"
